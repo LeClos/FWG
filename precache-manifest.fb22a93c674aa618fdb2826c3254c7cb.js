81ae77c14b74f9a20784829d98def780c57e18d6
LeClos <jared.leclos@gmail.com>
2019-08-27 10:21:09 +0800

    Updates

---
+++ precache-manifest.fb22a93c674aa618fdb2826c3254c7cb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b3afd0d740dec60e00ae0a2cd4e45c",
+    "url": "/FWG/index.html"
+  },
+  {
+    "revision": "43dfe5a007faedb9d508",
+    "url": "/FWG/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0839138527680754c46a",
+    "url": "/FWG/static/css/main.a47a5542.chunk.css"
+  },
+  {
+    "revision": "43dfe5a007faedb9d508",
+    "url": "/FWG/static/js/2.3f10252b.chunk.js"
+  },
+  {
+    "revision": "0839138527680754c46a",
+    "url": "/FWG/static/js/main.3ac87df8.chunk.js"
+  },
+  {
+    "revision": "a9f7ba6e62ed5719bab6",
+    "url": "/FWG/static/js/runtime~main.a66eaccb.js"
+  },
+  {
+    "revision": "2def3dfc9a0559f667c509c2ae22ed6e",
+    "url": "/FWG/static/media/paper.2def3dfc.png"
+  },
+  {
+    "revision": "927ee8f5be0b12c29508ce4e20bf55fc",
+    "url": "/FWG/static/media/rock.927ee8f5.jpg"
+  },
+  {
+    "revision": "fae642814fd95069fc7a01d2ef0ff855",
+    "url": "/FWG/static/media/scissors.fae64281.png"
+  }
+]);
